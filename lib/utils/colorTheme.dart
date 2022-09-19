f9d70b6a69655885a85381a7b1d8e94b1ee89a95
JirkaKlimes <jiriklimes920@gmail.com>
2022-09-19 15:00:17 +0200

    better color theme

--- lib/utils/colorTheme.dart
+++ lib/utils/colorTheme.dart
@@ -3,12 +3,16 @@ import 'package:spse_rozvrh/utils/shared_prefs.dart';
 
 class CustomColors {
   late Color primaryBkg;
-  late Color normalHour;
   late Color secondaryBkg;
+  late Color normalHour;
   late Color changedHour;
-  late Color selectedDay;
   late Color freeHour;
   late Color droppedHour;
+  late Color normalHourText;
+  late Color changedHourText;
+  late Color freeHourText;
+  late Color droppedHourText;
+  late Color selectedDay;
   late Color primaryText;
   late Color secondaryText;
   late Color currentDay;
@@ -18,34 +22,41 @@ class CustomColors {
   CustomColors() {
     switch (SharedPrefs().theme) {
       case "dark":
-        primaryBkg = const Color.fromARGB(255, 20, 24, 43);
+        primaryBkg = const Color.fromARGB(255, 27, 30, 47);
         secondaryBkg = const Color.fromARGB(255, 9, 12, 28);
-        normalHour = const Color.fromARGB(255, 20, 24, 43);
-        changedHour = const Color.fromARGB(255, 226, 118, 74);
-        freeHour = const Color.fromARGB(255, 91, 231, 154);
-        droppedHour = const Color.fromARGB(255, 91, 231, 154);
-        selectedDay = const Color.fromARGB(255, 66, 28, 255);
+        normalHour = const Color.fromARGB(255, 27, 30, 47);
+        changedHour = const Color.fromARGB(255, 27, 30, 47);
+        freeHour = const Color.fromARGB(255, 27, 30, 47);
+        droppedHour = const Color.fromARGB(255, 27, 30, 47);
+        normalHourText = const Color.fromARGB(255, 255, 255, 255);
+        changedHourText = const Color.fromARGB(255, 251, 122, 101);
+        freeHourText = const Color.fromARGB(255, 91, 231, 154);
+        droppedHourText = const Color.fromARGB(255, 91, 231, 154);
+        selectedDay = const Color.fromARGB(255, 67, 120, 255);
         primaryText = const Color.fromARGB(255, 255, 255, 255);
         secondaryText = const Color.fromARGB(255, 255, 255, 255);
         currentDay = const Color.fromARGB(255, 0, 255, 0);
         currentHour = const Color.fromARGB(255, 0, 255, 0);
-        shadows = const Color.fromARGB(255, 65, 124, 255).withOpacity(0.3);
+        shadows = const Color.fromARGB(255, 82, 137, 255).withOpacity(0.2);
         break;
 
       case "light":
-      
-        primaryBkg = const Color.fromARGB(255, 155, 193, 188);
-        secondaryBkg = const Color.fromARGB(255, 230, 235, 224);
-        normalHour = const Color.fromARGB(255, 92, 164, 169);
-        changedHour = const Color.fromARGB(255, 237, 106, 90);
-        freeHour = const Color.fromARGB(255, 244, 241, 187);
-        droppedHour = const Color.fromARGB(255, 244, 241, 187);
-        selectedDay = const Color.fromARGB(255, 155, 158, 151);
+        primaryBkg = const Color.fromARGB(255, 168, 168, 168);
+        secondaryBkg = const Color.fromARGB(255, 255, 255, 255);
+        normalHour = const Color.fromARGB(255, 168, 168, 168);
+        changedHour = const Color.fromARGB(255, 168, 168, 168);
+        freeHour = const Color.fromARGB(255, 168, 168, 168);
+        droppedHour = const Color.fromARGB(255, 168, 168, 168);
+        normalHourText = const Color.fromARGB(255, 255, 255, 255);
+        changedHourText = const Color.fromARGB(255, 190, 50, 28);
+        freeHourText = const Color.fromARGB(255, 91, 231, 154);
+        droppedHourText = const Color.fromARGB(255, 91, 231, 154);
+        selectedDay = const Color.fromARGB(255, 168, 168, 168);
         primaryText = const Color.fromARGB(255, 0, 0, 0);
-        secondaryText = const Color.fromARGB(255, 0, 0, 0);
+        secondaryText = const Color.fromARGB(255, 255, 255, 255);
         currentDay = const Color.fromARGB(255, 0, 255, 0);
         currentHour = const Color.fromARGB(255, 0, 255, 0);
-        shadows = const Color.fromARGB(255, 0, 0, 0).withOpacity(0.6);
+        shadows = const Color.fromARGB(255, 0, 0, 0).withOpacity(0.3);
         break;
     }
   }
